ef4000c6109865aeb603c6c61d5c60ce5b69b78b
サブPC <syou5963@outlook.jp>
2024-04-04 16:36:15 +0900

    apiの共通化

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,28 @@
+import axios from 'axios';
+
+// ログインを行う処理
+export const login = async (mail, password) => {
+    axios.post('http://localhost/api/login', {
+        email: mail,
+        password: password
+    })
+    // ログイン成功時の処理
+    .then(response => {
+        if(response.status === 200){
+            console.log("トークン："+response.data.token);
+            alert('フロント：ログインに成功しました');// ログイン成功をユーザーに通知
+            window.location.href = '/openchat'; // 遷移先のURLにリダイレクト
+        } else if(response.status === 401) alert("サーバー:" + response.error);// 認証失敗時  
+        else alert("フロント：予期せぬエラーが発生してしまいました。");
+    })
+    // ログイン失敗時の処理
+    .catch(error => {
+        console.error('ログインエラー:', error);
+        alert('ログインに失敗しました。'+ error.response.statusText);
+    });
+}
+
 export const fetchData = async () => {
-    //API一覧
-    //ToDo一覧の取得
-    //http://localhost/api/toDos
-    //投稿一覧の取得
-    //http://localhost/api/post
     try {
         const response = await fetch('http://localhost/api/post');
         const data = await response.json();
