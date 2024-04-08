f9facb70f32cfd05b439637f969d08ca1524e1e7
サブPC <syou5963@outlook.jp>
2024-04-08 13:54:54 +0900

    未認証の場合のページ遷移制御を仮で実装

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -6,37 +6,48 @@ import { isTokenCheck } from '../../api/api';
 export const Header = () => {
   const [isLoggedIn, setLoggedIn] = useState(); // ログイン状態を管理するためのstate
 
+  const loginAlert = () => {
+    alert("ログインしてください");
+  }
+
   useEffect(() => {
     const checkToken = async () => {
       const tokenCheck = await isTokenCheck();
       setLoggedIn(tokenCheck);
     };
-
     checkToken();
   }, []);
   return (
     <AppBar position="static">
       <Toolbar>
-        <Button color="inherit" component={Link} to="/">
-          Home
-        </Button>
-        <Button color="inherit" component={Link} to="/openchat">
-          Open Chat
-        </Button>
-        <Button color="inherit" component={Link} to="/privatechat">
-          Private Chat
-        </Button>
-        <Button color="inherit" component={Link} to="/dm">
-          DM
-        </Button>
         {isLoggedIn ? (
-          <Button color="inherit" component={Link} to="/mypage">
-            MyPage
-          </Button>
+        <Button color="inherit" component={Link} to="/">Home</Button>
         ) : (
-          <Button color="inherit" component={Link} to="/login">
-            Login
-          </Button>
+        <Button color="inherit" onClick={loginAlert}>Home</Button>
+        )}
+
+        {isLoggedIn ? (
+        <Button color="inherit" component={Link} to="/openchat">Open Chat</Button>
+        ) : (
+        <Button color="inherit" onClick={loginAlert}>Open Chat</Button>
+        )}
+
+        {isLoggedIn ? (
+        <Button color="inherit" component={Link} to="/privatechat">Private Chat</Button>
+        ) : (
+        <Button color="inherit" onClick={loginAlert}>Private Chat</Button>
+        )}
+
+        {isLoggedIn ? (
+        <Button color="inherit" component={Link} to="/dm">DM</Button>
+        ) : (
+        <Button color="inherit" onClick={loginAlert}>DM</Button>
+        )}
+
+        {isLoggedIn ? (
+        <Button color="inherit" component={Link} to="/mypage">MyPage</Button>
+        ) : (
+        <Button color="inherit" component={Link} to="/login">Login</Button>
         )}
       </Toolbar>
     </AppBar>
